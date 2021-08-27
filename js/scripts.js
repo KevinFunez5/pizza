0d20e88e7fbf77eb5dfd8762be3eb061b277f884
Kevin Funez <kevin.funez315@gmail.com>
2021-08-27 11:00:47 -0700

    add values to consts

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+const SIZES = [
+  { size: "small", price: 5.00},
+  { size: "medium", price: 10.00},
+  { size: "large", price: 12.00},
+  { size: "x-large", price: 15.00}
+];
+
+const TOPPINGS = [
+  { size: "pepperoni", price: 1.00},
+  { size: "sausage", price: 1.00},
+  { size: "pineapple", price: 1.00},
+  { size: "jalapenos", price: 1.00}
+];
